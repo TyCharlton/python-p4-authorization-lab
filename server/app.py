3a80f1093cc44811caedecba212cd17d2b7f7f7f
TylerCharlton <TyCharlton1993@gmail.com>
2024-02-29 20:33:52 -0500

    completed lab

--- server/app.py
+++ server/app.py
@@ -87,12 +87,18 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        if not session['user_id']:
+            return {'message': 'use must be logged in'}, 401
+        articles = [article.to_dict() for article in Article.query.filter(Article.is_member_only == True).all()]
+        return make_response(articles, 200)
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        if not session['user_id']:
+            return {'message': 'use must be logged in'}, 401
+        article = Article.query.filter(Article.id == id).first()
+        return make_response(article.to_dict(), 200)
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
